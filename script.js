92e09cb1106fc11956cbd4450464fe7329c6b6af
3046983449hc-wq <3046983449hc@gmail.com>
2026-07-03 21:32:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 window.onload = function () {
-    document.body.style.background = "white";
-    document.body.style.color = "black";
-    document.body.style.zIndex = "9999";
+    const p = document.getElementById("status");
 
-    document.body.innerHTML = `
-        <div style="position:relative; z-index:9999;">
-            <h1>TEST OK ✅</h1>
-            <p>If you see this, JS is working</p>
-        </div>
-    `;
+    if (p) {
+        p.innerText = "Rain system loaded successfully";
+    }
 };
